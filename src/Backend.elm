85ee0a69bb8cac380f35a214922d8bfe6377fa66
Alex <alexmclm@gmail.com>
2019-08-09 12:17:18 -0300

    agregue 2 funciones y no rompio, considero que esta casi bien jajaaj

--- src/Backend.elm
+++ src/Backend.elm
@@ -22,12 +22,20 @@ import String exposing (concat)
 
 -- Debería darnos la url de la cancion en base al id
 urlById : String -> List Song -> String
-urlById id songs = (List.filter (id == idCancion songs) songs).url -- no se me ocurre como meter la url de cada cancion
+urlById id songs = (findSong (esLaMismaId id) songs).url -- use la funcion que esta arriba jajaajaj
+-- no se me ocurre como meter la url de cada cancion
 --filtro por aquellos que sean igual al id que tengo, pero nose como llamar al id sin mencionarlo
+--  (List.filter (id == idCancion songs) songs).url
 
-idCancion : List Song -> List String
-idCancion songs = List.map (id) songs -- esta cagada no quiere funcionar, quiero mandarle el id a cada una de las canciones
+esLaMismaId : String -> Song -> Bool
+esLaMismaId id song = id == song.id
+
+--idCancion : List Song -> List String
+--idCancion songs =  []
+-- List.map (id) songs -- esta cagada no quiere funcionar, quiero mandarle el id a cada una de las canciones
 -- asi me devuelve una lista de ids
+-- List.map (id) songs
+
 
 -- Debería darnos las canciones que tengan ese texto en nombre o artista
 filterByName : String -> List Song -> List Song
@@ -47,22 +55,25 @@ toggleLike id songs = []
 -- hay que arreglarla
 isLiked : Song  -> Bool
 isLiked song = song.liked -- aparentemente deberia ser asi
--- song tiene:
+
 
 -- Recibe una lista de canciones y nos quedamos solo con las que
 -- tienen un like
 filterLiked : List Song -> List Song
-filterLiked songs = []  -- List.filter (map isLiked<<song) songs -- no creo que este bien...
+filterLiked songs = List.filter isLiked songs 
 
 -- Agrega una cancion a la cola de reproduccion
 -- (NO es necesario preocuparse porque este una sola vez)
 addSongToQueue : Song -> List Song -> List Song
-addSongToQueue song queue = queue
+addSongToQueue song queue = queue++[song]
 
 -- Saca una cancion de la cola
 -- (NO es necesario que se elimine una sola vez si esta repetida)
 removeSongFromQueue : String -> List Song -> List Song
-removeSongFromQueue id queue = queue
+removeSongFromQueue id queue = List.filter (esLaCancion id) queue
+
+esLaCancion : String -> Song -> Bool
+esLaCancion id queue = not(esLaMismaId id queue) -- si no estamos hablado del msimo tema, filtramelo de la lista
 
 -- Hace que se reproduzca la canción que sigue y la saca de la cola
 playNextFromQueue : Model -> Model
